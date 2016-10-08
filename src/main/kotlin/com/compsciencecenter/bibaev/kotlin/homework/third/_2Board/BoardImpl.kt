4a4c39d732cc5c86d05c007d13422a1f4106f7e6
Vitaliy.Bibaev <roenke54@gmail.com>
2016-10-08 14:48:37 +0300

    Implement GameBoard class

--- src/main/kotlin/com/compsciencecenter/bibaev/kotlin/homework/third/_2Board/BoardImpl.kt
+++ src/main/kotlin/com/compsciencecenter/bibaev/kotlin/homework/third/_2Board/BoardImpl.kt
@@ -2,65 +2,78 @@ package com.compsciencecenter.bibaev.kotlin.homework.third._2Board
 
 open class SquareBoardImpl(override val width: Int) : SquareBoard {
     override fun getCell(i: Int, j: Int): Cell {
-        throw UnsupportedOperationException("not implemented") //To change body of created functions use File | Settings | File Templates.
+        if (checkBounds(i)) {
+            if (checkBounds(j)) {
+                return CellImpl(i, j)
+            }
+            throw IllegalArgumentException("Index \"j\" should be from 1 to $width inclusive")
+        }
+
+        throw IllegalArgumentException("Index \'i\' should be from 1 fo $width inclusive")
     }
 
     override fun getCellOrNull(i: Int, j: Int): Cell? {
-        throw UnsupportedOperationException("not implemented") //To change body of created functions use File | Settings | File Templates.
+        if (checkBounds(i) && checkBounds(j)) {
+            return CellImpl(i, j)
+        }
+
+        return null
     }
 
     override fun getAllCells(): Collection<Cell> {
-        throw UnsupportedOperationException("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val result = mutableListOf<Cell>()
+        for (i in 1..width) {
+            for (j in 1..width) {
+                result.add(CellImpl(i, j))
+            }
+        }
+
+        return result
     }
 
-    override fun getRow(i: Int, jRange: IntProgression): List<Cell> {
-        throw UnsupportedOperationException("not implemented") //To change body of created functions use File | Settings | File Templates.
+    override fun getRow(i: Int, jRange: IntProgression): List<Cell> = jRange.map { j -> CellImpl(i, j) }.toList()
+
+    override fun getColumn(iRange: IntProgression, j: Int): List<Cell> = iRange.map { i -> CellImpl(i, j) }.toList()
+
+
+    override fun Cell.getNeighbour(direction: Direction): Cell? = when (direction) {
+        Direction.UP -> getCellOrNull(i - 1, j)
+        Direction.DOWN -> getCellOrNull(i + 1, j)
+        Direction.RIGHT -> getCellOrNull(i, j + 1)
+        Direction.LEFT -> getCellOrNull(i, j - 1)
     }
 
-    override fun getColumn(iRange: IntProgression, j: Int): List<Cell> {
-        throw UnsupportedOperationException("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
+    private fun checkBounds(index: Int): Boolean = 1 <= index && index <= width
 
-    override fun Cell.getNeighbour(direction: Direction): Cell? {
-        throw UnsupportedOperationException("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
+    protected data class CellImpl(override val i: Int, override val j: Int) : Cell
 }
 
 class GameBoardImpl<T>(override val width: Int) : SquareBoardImpl(width), GameBoard<T> {
-    override fun get(cell: Cell): T? {
-        throw UnsupportedOperationException("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
+    private val map: MutableMap<Cell, T> = mutableMapOf()
+    override fun get(cell: Cell): T? = map[cell]
 
     override fun set(cell: Cell, value: T?) {
-        throw UnsupportedOperationException("not implemented") //To change body of created functions use File | Settings | File Templates.
+        if (value == null) {
+            map.remove(cell)
+        } else {
+            map[cell] = value
+        }
     }
 
-    override fun get(i: Int, j: Int): T? {
-        throw UnsupportedOperationException("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
+    override fun get(i: Int, j: Int): T? = get(CellImpl(i, j))
 
-    override fun set(i: Int, j: Int, value: T?) {
-        throw UnsupportedOperationException("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
+    override fun set(i: Int, j: Int, value: T?) = set(CellImpl(i, j), value)
 
-    override fun contains(value: T): Boolean {
-        throw UnsupportedOperationException("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
+    override fun contains(value: T): Boolean = map.values.contains(value)
 
-    override fun filter(predicate: (T?) -> Boolean): Collection<Cell> {
-        throw UnsupportedOperationException("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
+    override fun filter(predicate: (T?) -> Boolean): Collection<Cell> =
+            getAllCells().filter { cell -> predicate(get(cell)) }
 
-    override fun any(predicate: (T?) -> Boolean): Boolean {
-        throw UnsupportedOperationException("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
+    override fun any(predicate: (T?) -> Boolean): Boolean = getAllCells().any { cell -> predicate(get(cell)) }
 
-    override fun all(predicate: (T?) -> Boolean): Boolean {
-        throw UnsupportedOperationException("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
+    override fun all(predicate: (T?) -> Boolean): Boolean = getAllCells().all { cell -> predicate(get(cell)) }
 }
 
 fun createSquareBoard(width: Int): SquareBoard = SquareBoardImpl(width)
 
-fun <T> createGameBoard(width: Int): GameBoard<T> = GameBoardImpl<T>(width)
+fun <T> createGameBoard(width: Int): GameBoard<T> = GameBoardImpl(width)
