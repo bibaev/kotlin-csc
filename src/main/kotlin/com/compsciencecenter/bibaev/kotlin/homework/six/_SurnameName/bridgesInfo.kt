b8bb51123342c6922160eb65aa04cb9367c0e9be
roenke <roenke54@gmail.com>
2016-11-22 14:12:09 +0300

    Implement DSL for bridges

--- src/main/kotlin/com/compsciencecenter/bibaev/kotlin/homework/six/_SurnameName/bridgesInfo.kt
+++ src/main/kotlin/com/compsciencecenter/bibaev/kotlin/homework/six/_SurnameName/bridgesInfo.kt
@@ -1,20 +1,38 @@
 package _SurnameName
 
-import bridges.BridgeState
+import bridges.*
 import bridges.BridgeState.CLOSED
-import bridges.BridgesInfo
-import bridges.SwitchAction
 import bridges.SwitchAction.CHANGE
-import bridges.SwitchType
 import bridges.SwitchType.HALF_BLOCK
 
 // Your solution should live in this folder/package only (rename _SurnameName accordingly.)
 // You may add as many subpackages as you need, but the function 'bridgesInfo' below should live in the root _SurnameName package.
 // Please DON'T copy the class 'BridgesInfo' and others here.
 
-fun bridgesInfo(init: () -> Unit): BridgesInfo = TODO()
+fun bridgesInfo(init: BridgesBuilder.() -> Unit): BridgesInfo {
+    val bridges = mutableMapOf<Char, Bridge>()
+    val switches = mutableMapOf<Char, Switch>()
 
-// These functions need receivers to works correctly. The declarations below are only used to have the compiled code.
+    object : BridgesBuilder {
+        override fun bridge(name: Char, initialState: BridgeState, init: BridgeBuilder.() -> Unit) {
+            val bridge = Bridge(name, initialState)
+            object : BridgeBuilder {
+                override fun switch(name: Char, action: SwitchAction, type: SwitchType) {
+                    switches[name] = Switch(name, bridge, action, type)
+                }
 
-fun bridge(name: Char, initialState: BridgeState = CLOSED, init: () -> Unit = {}) { TODO() }
-fun switch(name: Char, action: SwitchAction = CHANGE, type: SwitchType = HALF_BLOCK) { TODO() }
+            }.init()
+            bridges[name] = bridge
+        }
+    }.init()
+
+    return BridgesInfo(bridges, switches)
+}
+
+interface BridgesBuilder {
+    fun bridge(name: Char, initialState: BridgeState = CLOSED, init: BridgeBuilder.() -> Unit = {})
+}
+
+interface BridgeBuilder {
+    fun switch(name: Char, action: SwitchAction = CHANGE, type: SwitchType = HALF_BLOCK)
+}
